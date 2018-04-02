894218d5b0b35459e3365a2979fe27a29b9c8644
HANGHANG <hcszheng@163.com>
2018-04-02 09:48:27 +0800

    add menu and layout

--- app/src/main/java/com/example/hcszh/usb1_0/MainActivity.java
+++ app/src/main/java/com/example/hcszh/usb1_0/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.hcszh.usb1_0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "hello");
+        Button button1 = (Button) findViewById(R.id.button_open);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "正在打开文件",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
